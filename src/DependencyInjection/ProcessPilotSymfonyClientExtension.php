fe8b90078a16dacd0d8171874de36175469c7202
Sebastian Hopfe <s.hopfe@me.com>
2024-01-14 20:38:50 +0100

    Initialize Symfony services configuration
    Added a new 'services.xml' file for Symfony service configuration. Additionally, updated 'ProcessPilotSymfonyClientExtension.php' to load this services configuration file, replacing the previous debug statement. This will allow for better utilization of Symfony's dependency injection framework.

--- src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
+++ src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
@@ -2,6 +2,8 @@
 
 namespace ProcessPilot\Symfony\Client\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -9,6 +11,12 @@ final class ProcessPilotSymfonyClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        var_dump('We\'re alive!');die;
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(
+                dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'config'
+            )
+        );
+        $loader->load('services.xml');
     }
 }
