1223478182092caab7a4d1b8f7e9deaa808e5a65
Sebastian Hopfe <s.hopfe@me.com>
2024-01-14 20:33:49 +0100

    Add Symfony dependency injection to composer.json
    This commit includes the "symfony/dependency-injection" package in the composer.json file, impacting the entire application. Also, updated the ProcessPilotSymfonyClientExtension to use this package, to manage dependencies more freely and conveniently throughout the application.

--- src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
+++ src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
@@ -3,10 +3,11 @@
 namespace ProcessPilot\Symfony\Client\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class ProcessPilotSymfonyClientExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container): void
+    public function load(array $configs, ContainerBuilder $container)
     {
         var_dump('We\'re alive!');die;
     }
