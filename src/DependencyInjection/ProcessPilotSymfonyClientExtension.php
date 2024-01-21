43cd64a7745d8131f32c14466ff4e40b79b0a059
Sebastian Hopfe <s.hopfe@me.com>
2024-01-21 16:56:33 +0100

    The commit message the AI produced should actually be:
    Make ProcessPilotSymfonyClientExtension a final class and add alias
    
    The ProcessPilotSymfonyClientExtension class has been made final to prevent further inheritance. An 'alias' method has also been added to this class, returning 'process_pilot' in order to simplify and consolidate the configuration process.

--- src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
+++ src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
@@ -7,11 +7,16 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class ProcessPilotSymfonyClientExtension extends Extension
+final class ProcessPilotSymfonyClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__, 2) . '/config/'));
         $loader->load('services.xml');
+    }
+
+    public function getAlias(): string
+    {
+        return 'process_pilot';
     }
 }
