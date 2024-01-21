bb34fe69d2a4d81a68cc1abb0b08579a15e1b1fe
Sebastian Hopfe <s.hopfe@me.com>
2024-01-21 16:46:49 +0100

    Refactor dependency injection in ProcessPilotSymfonyClientExtension
    In an effort to simplify the project structure, we've updated 'ProcessPilotSymfonyClientExtension' to handle the dependency injection directly. This removes the need for two separate classes and shifts the XML file loading to this main class. This streamlined configuration process potentially simplifies updates and maintenance in the future.

--- src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
+++ src/DependencyInjection/ProcessPilotSymfonyClientExtension.php
@@ -2,13 +2,16 @@
 
 namespace ProcessPilot\Symfony\Client\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ProcessPilotSymfonyClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        var_dump('we are alive'); die();
+        $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__, 2) . '/config/'));
+        $loader->load('services.xml');
     }
 }
