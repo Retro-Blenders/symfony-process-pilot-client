f0fbc671b0342f45a9a635668ca9e31d68207ed5
Sebastian Hopfe <s.hopfe@me.com>
2024-01-21 16:09:23 +0100

    Add getExtensionAlias method in ProcessPilotSymfonyClientBundle
    We added a new method, getExtensionAlias, to the ProcessPilotSymfonyClientBundle class. This method returns the alias for the bundle, which is crucial for Symfony's naming convention and bundle registration. It results in improved readability and easier bundle management in the Symfony project.

--- src/ProcessPilotSymfonyClientBundle.php
+++ src/ProcessPilotSymfonyClientBundle.php
@@ -6,5 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ProcessPilotSymfonyClientBundle extends AbstractBundle
 {
-
+    /**
+     * @return string
+     */
+    public function getExtensionAlias(): string
+    {
+        return 'process-pilot';
+    }
 }
