523c045cb299017bf5f24d8b815ea2f6dc3e9ebc
Sebastian Hopfe <s.hopfe@me.com>
2024-01-21 16:26:53 +0100

    Refactor Dependency Injection configuration into main bundle class
    Two Dependency Injection related classes have been deleted and their functionality is directly integrated in the ProcessPilotSymfonyClientBundle class. It simplifies the structure of the project and makes the configuration process more straightforward. The changes should make future updates and maintenance easier.

--- src/ProcessPilotSymfonyClientBundle.php
+++ src/ProcessPilotSymfonyClientBundle.php
@@ -3,14 +3,34 @@
 namespace ProcessPilot\Symfony\Client;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ProcessPilotSymfonyClientBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->getRootNode()
+            ->children()
+                ->scalarNode('host')->end()
+                ->scalarNode('project_id')->end()
+                ->scalarNode('project_hash')->end()
+                ->booleanNode('enabled')->defaultTrue()->end()
+            ->end()
+        ;
+    }
+
     /**
      * @return string
      */
     public function getExtensionAlias(): string
     {
         return 'process-pilot';
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        dd($config);
     }
 }
