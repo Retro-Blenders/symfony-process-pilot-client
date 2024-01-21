807850075de583ad154d0da3b3aa7a38f82bdbcb
Sebastian Hopfe <s.hopfe@me.com>
2024-01-21 17:01:11 +0100

    Add getContainerExtension to ProcessPilotSymfonyClientBundle
    Introduced a getContainerExtension method in the ProcessPilotSymfonyClientBundle class. This method instantiates and returns a ProcessPilotSymfonyClientExtension. This enables our Symfony bundle to support extension loading, enhancing the customizability and scalability of our application code.

--- src/ProcessPilotSymfonyClientBundle.php
+++ src/ProcessPilotSymfonyClientBundle.php
@@ -2,14 +2,25 @@
 
 namespace ProcessPilot\Symfony\Client;
 
+use ProcessPilot\Symfony\Client\DependencyInjection\ProcessPilotSymfonyClientExtension;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ProcessPilotSymfonyClientBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new ProcessPilotSymfonyClientExtension();
+        }
+
+        return $this->extension;
+    }
+
 //    public function configure(DefinitionConfigurator $definition): void
 //    {
 //        $definition->rootNode()
