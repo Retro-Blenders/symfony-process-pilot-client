9fff17145763633295692b8da22b5e02cf4a9ca8
Sebastian Hopfe <s.hopfe@me.com>
2024-01-21 22:16:37 +0100

    Implement boot method and add return type
    A boot method is added to src/ProcessPilotSymfonyClientBundle.php to instantiate Settings, PilotClientService, and ErrorHandler when the bundle is booted. A return type `void` is also added to the load function in ProcessPilotSymfonyClientExtension.php for stricter type checking.

--- src/ProcessPilotSymfonyClientBundle.php
+++ src/ProcessPilotSymfonyClientBundle.php
@@ -2,6 +2,9 @@
 
 namespace ProcessPilot\Symfony\Client;
 
+use ProcessPilot\Client\ErrorHandler;
+use ProcessPilot\Client\Service\PilotClientService;
+use ProcessPilot\Client\Settings;
 use ProcessPilot\Symfony\Client\DependencyInjection\ProcessPilotSymfonyClientExtension;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -15,5 +18,16 @@ final class ProcessPilotSymfonyClientBundle extends Bundle
         }
 
         return $this->extension;
+    }
+
+    public function boot(): void
+    {
+        /** @var Settings $settings */
+        $settings = $this->container->get(Settings::class);
+
+        $pilotClientService = PilotClientService::getInstance();
+        $pilotClientService::setSettings($settings);
+
+        ErrorHandler::register();
     }
 }
